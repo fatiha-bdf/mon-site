658f5740ea2a74c2115b9e463216a76329e5b860
fatiha-bdf <fatiha.boudyaf@gmail.com>
2022-09-28 13:39:00 +0200

    5

--- client/src/components/sections/AboutMe.js
+++ client/src/components/sections/AboutMe.js
@@ -12,6 +12,10 @@ function AboutMe() {
 				</div>
 				<div className = 'about'>
 				<br/>
+				<h5>Bienvenue. Ce site a été concus avec les technologies ReactJS et NodeJS. Vous pourrez consulter le code source sur mon github.</h5>
+				<h5>N'hésitez pas à me contacter en cliquant sur le bouton "Contactez moi" qui se trouve ci-dessus.</h5>
+				<br/>
+				<h4><strong>À propos de moi</strong></h4>
 				<h5>Développeuse <strong>curieuse et remplie d'energie.</strong></h5>
 				<h5>Passionnée par l’innovation, j'aime relever les <strong>challenges en équipe</strong>, et penser au delà des <strong>limites</strong> fixées.</h5>
 				<h5>Je désire aujourd'hui mettre mon savoir en pratique dans un projet stimulant au sein d'une équipe motivée par le challenge.</h5>
