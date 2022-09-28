351efab7d5051f9126354292fc8464622375540d
fatiha-bdf <fatiha.boudyaf@gmail.com>
2022-09-28 14:52:24 +0200

    .

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path')
 app.use(cors())
 
 app.use((req, res, next) => {
+
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
